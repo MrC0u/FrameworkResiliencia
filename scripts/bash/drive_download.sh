269b2199a8fb97179a18d6fa9a93b446559414f3
MrC0u <marcosdvalderrama@gmail.com>
2024-05-28 22:00:30 -0400

    -

--- scripts/bash/drive_download.sh
+++ scripts/bash/drive_download.sh
@@ -11,9 +11,8 @@ FILE_ID="$1"
 CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=${FILE_ID}" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p')
 DOWNLOAD_URL=$(wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" --max-redirect=0 2>&1 | grep "Location" | cut -d " " -f 2)
 
-# Descargar el archivo utilizando axel
-axel -a --header "Cookie: $(cat /tmp/cookies.txt | grep 'DRIVE_STREAM' | cut -d$'\t' -f 7)" "$DOWNLOAD_URL"
+wget --load-cookies /tmp/cookies.txt "$DOWNLOAD_URL"
 rm -f /tmp/cookies.txt
 
 # Mostrar la ubicación del archivo descargado
-echo "Archivo guardado en: $(pwd)/"
+echo "Archivo guardado como: $(basename "$DOWNLOAD_URL")"
