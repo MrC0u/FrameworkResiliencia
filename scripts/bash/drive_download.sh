d86f97fd19dc81616bfb6dc5ae268ff93986aae6
MrC0u <marcosdvalderrama@gmail.com>
2024-05-28 22:33:24 -0400

    -

--- scripts/bash/drive_download.sh
+++ scripts/bash/drive_download.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Error - Falta el parametro: <ID>"
+    echo "Error - Faltan el parametro: <ID>"
     echo "[Ejemplo: gdrive-download <id>]"
     exit 1
 fi
 
 FILE_ID="$1"
 
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=${FILE_ID}" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p')
-DOWNLOAD_URL=$(wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" --max-redirect=0 2>&1 | grep "Location" | cut -d " " -f 2)
-FILENAME=$(basename "$DOWNLOAD_URL")
+axel -o "$FILENAME" --insecure "https://docs.google.com/uc?export=download&id=$FILE_ID"
 
-wget --load-cookies /tmp/cookies.txt -O "$FILENAME" "$DOWNLOAD_URL"
-rm -f /tmp/cookies.txt
-
-echo "Archivo guardado en: $(pwd)/$FILENAME"
-
+echo "Archivo guardado en: $(pwd)/"
